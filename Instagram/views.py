909fec71df6b090dd0d8bb063c6e5e766951f67d
joankirui <joankirui99@gmail.com>
2021-12-03 13:48:39 +0300

    create all templates I need

--- Instagram/views.py
+++ Instagram/views.py
@@ -5,4 +5,6 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='/accounts/login')
 def index(request):
+    title = 'insta-clone'
+    return render(request,'index.html',{"title":title})
 
