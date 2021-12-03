a361e6400746fa39dd1a0b5623ab425780277e95
joankirui <joankirui99@gmail.com>
2021-12-03 13:53:43 +0300

    update base.html

--- Instagram/views.py
+++ Instagram/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url='/accounts/login')
+# @login_required(login_url='/accounts/login')
 def index(request):
     title = 'insta-clone'
     return render(request,'index.html',{"title":title})
