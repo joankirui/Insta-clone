f1ed5bca6bb70b26eb451fa44aa6198b5b72ec76
joankirui <joankirui99@gmail.com>
2021-12-05 20:55:56 +0300

    update forms.py

---
+++ Instagram/forms.py
@@ -0,0 +1,45 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from django.contrib.auth.models import User
+from .models import *
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+
+class EditProfileForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['prof_image', 'bio']
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
+
+
+class PostPicForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['image', 'img_caption']
--- Instagram/views.py
+++ Instagram/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from Instagram.forms import CommentForm
 from Instagram.models import Image,Comment,Likes,Profile
 
 
