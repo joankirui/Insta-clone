ae8145890269b90d13cdefbfe71b7bdc8b2e3a6a
joankirui <joankirui99@gmail.com>
2021-12-05 14:13:13 +0300

    add profile model

--- Instagram/models.py
+++ Instagram/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    prof_image = CloudinaryField('image',null=True)
+    bio = models.CharField(max_length=30)
