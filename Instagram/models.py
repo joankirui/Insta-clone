c709f5c8137c37bfe36e61e259b9fbf8864adda4
joankirui <joankirui99@gmail.com>
2021-12-05 14:24:32 +0300

    add methods for class Profile

--- Instagram/models.py
+++ Instagram/models.py
@@ -7,3 +7,15 @@ class Profile(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     prof_image = CloudinaryField('image',null=True)
     bio = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.user.username
+    
+    def save_profile(self):
+        self.save()
+
+    def update_profile(self):
+        self.update_profile()
+
+    def delete_profile(self):
+        self.delete()
